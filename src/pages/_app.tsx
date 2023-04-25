e3ad4dfe6817cff0f781944f58cb6581ba651cd6
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-25 17:50:23 -0300

    colocando o GLobalStyle no local correto e criando mais cores no global css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import type { AppProps } from 'next/app'
 import { GlobalStyles } from '../styles/global'
 
+GlobalStyles();
 export default function App({ Component, pageProps }: AppProps) {
-  GlobalStyles();
+
   return <Component {...pageProps} />
 }

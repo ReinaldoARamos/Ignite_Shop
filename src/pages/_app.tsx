3ebc6145158be0bbb62c68511317d87286898c93
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-26 16:55:24 -0300

    criando a next Image future

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,11 @@ import type { AppProps } from 'next/app'
 import { GlobalStyles } from '../styles/global'
 import Logo from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/app';
+
+import Image from 'next/image';
+
 GlobalStyles();
+
 export default function App({ Component, pageProps }: AppProps) {
 
   return (

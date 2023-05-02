60c0ed89af9b0b81d7e04ea08b763e73e909331f
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-02 15:40:21 -0300

    arrumando font que nao tinha Roboto e sim Nunito

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,19 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap"rel="stylesheet"/>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
-        <NextScript  />
+        <NextScript />
       </body>
     </Html>
   );

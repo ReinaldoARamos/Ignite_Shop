4978c16ef509ba628833cfb37734a07d0a02489a
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-03 16:00:41 -0300

    criando o fallback true para poder gerar outras páginas sem a ID passada no path

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,15 @@
 import { stripe } from "../../lib/stripe";
 
-import {GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 import Stripe from "stripe";
-import { ParsedUrlQuery } from 'querystring';
-import { ProductContainer, ImageContainer, ProductDetails } from "../../styles/pages/products";
+import { ParsedUrlQuery } from "querystring";
+import {
+  ProductContainer,
+  ImageContainer,
+  ProductDetails,
+} from "../../styles/pages/products";
+import { useRouter } from "next/router";
 
 /** const { isFallback } = useRouter();
   if (isFallback) {
@@ -13,17 +18,20 @@ import { ProductContainer, ImageContainer, ProductDetails } from "../../styles/p
    */
 
 interface ProductsProps {
-    products: {
-      id: string;
-      name: string;
-      imageURL: string;
-      price: string;
-      description: string;
-    };
-  } 
+  products: {
+    id: string;
+    name: string;
+    imageURL: string;
+    price: string;
+    description: string;
+  };
+}
 
 export default function Product({ products }: ProductsProps) {
- 
+  const { isFallback } = useRouter();
+  if (isFallback) {
+    return <p>Loading...</p>;
+  }
   return (
     <ProductContainer>
       <ImageContainer>
@@ -32,32 +40,28 @@ export default function Product({ products }: ProductsProps) {
       <ProductDetails>
         <h1> {products.name}</h1>
         <span>{products.price}</span>
-        <p>
-          {products.description}
-        </p>
+        <p>{products.description}</p>
         <button>Comprar agora!</button>
       </ProductDetails>
     </ProductContainer>
   );
 }
 
-export const getStaticPaths : GetStaticPaths = async () => { 
-return { 
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
     paths: [
       {
         params: {
-          id: 'prod_NnBi8kGskbRrBU',
+          id: "prod_NnBi8kGskbRrBU",
         },
       },
     ],
-    fallback: false,
+    fallback: true,
   };
-}
-
-
+};
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
-  params
+  params,
 }) => {
   //const productId = params.id;
   const productId = (params as ParsedUrlQuery).id;

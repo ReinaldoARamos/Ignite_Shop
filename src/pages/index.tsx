11e277e9b74db7d71595133eb41b77d41a1cb05b
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-03 14:24:38 -0300

    arrumando objeto que estava escrito no singular, fazendo o retorno ser undefined nos valores do produto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,6 +44,7 @@ export default function Home({ products }: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
+  
   const response = await stripe.products.list({
     expand: ["data.default_price"],
   });
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,24 +1,74 @@
-import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/products"
-import { GetStaticProps } from "next"
-import Image from "next/image"
-import { useRouter } from "next/router"
+import { stripe } from "../../lib/stripe";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/src/styles/pages/products";
+import { GetServerSideProps, GetStaticProps } from "next";
+import Image from "next/image";
+import Stripe from "stripe";
 
-export default function Product() {
-    const {query} = useRouter()
-    return (
-       <ProductContainer>
-        <ImageContainer >
-       
-        </ImageContainer>
-        <ProductDetails>
-      
-            <h1> Camisa X</h1>
-            <span>R$: 79.90</span>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos quo eaque, reprehenderit, accusamus quam tenetur ipsam quisquam hic sequi at eveniet. Ullam aperiam officiis vel maiores odio illum cumque expedita?</p>
-            <button>
-                Comprar agora!
-            </button>
-        </ProductDetails>
-       </ProductContainer>
-    )
+
+/** const { isFallback } = useRouter();
+  if (isFallback) {
+    return "loading";
+  }
+   */
+
+interface ProductsProps {
+    products: {
+      id: string;
+      name: string;
+      imageURL: string;
+      price: string;
+      description: string;
+    };
+  } 
+
+export default function Product({ products }: ProductsProps) {
+ 
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={products.imageURL} alt="" width={520} height={400}></Image>
+      </ImageContainer>
+      <ProductDetails>
+        <h1> {products.name}</h1>
+        <span>{products.price}</span>
+        <p>
+          {products.description}
+        </p>
+        <button>Comprar agora!</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps<any, { id: string }> = async ({
+  params
+}) => {
+  const productId = params.id;
+
+  const products = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+  const price = products.default_price as Stripe.Price;
+
+  return {
+    props: {
+      products: {
+        id: products.id,
+        name: products.name,
+        description: products.description,
+        imageURL: products.images[0],
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+          unitDisplay: "long",
+          maximumFractionDigits: 2,
+        }).format((price.unit_amount as number) / 100),
+      },
+    },
+    //revalidate: 60 * 60 * 1,
+  };
+};

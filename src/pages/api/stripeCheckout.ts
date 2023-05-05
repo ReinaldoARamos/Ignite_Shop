b7a9242a5eb36be2110aa0bb3fc3cdc6e2024357
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-05 15:48:41 -0300

    criando as urls de sucess e cancel

--- src/pages/api/stripeCheckout.ts
+++ src/pages/api/stripeCheckout.ts
@@ -3,7 +3,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse)  {
-
+  const sucessUrl = `${process.env.NEXT_URL}/sucess`
+  const cancelUrl = `${process.env.NEXT_URL}/cancel`
   const priceId = ''
   const checkoutsession = await stripe.checkout.create({
     cancel_url: '',

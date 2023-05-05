61bad4d03cfa28816adbee62fd971f7a1cb30278
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-05 15:41:01 -0300

    configurando a api do stripe para fazer uma compra na ignite_shop

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,19 @@
+import { stripe } from "@/src/lib/stripe";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse)  {
-  return res.json({ message: 'hello'})
+export default async function handler(req: NextApiRequest, res: NextApiResponse)  {
+
+  const priceId = ''
+  const checkoutsession = await stripe.checkout.create({
+    cancel_url: '',
+    sucess_url: '',
+    mode: 'payment',
+    lineItems: [
+      {
+        price: priceId,
+        quantity: 1
+      }
+    ]
+  })
 };

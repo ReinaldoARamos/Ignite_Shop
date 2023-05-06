2dcce45b95379b3a0d3385189d9b34813827b3f5
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-06 17:18:02 -0300

    colocando validação de priceid e também forçando a rota da API ser acessada somente via POST

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -7,6 +7,19 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const {priceId} = req.body
+
+
+  if(req.method != 'POST') {
+    return res.status(405).json({
+      error: "Method Not Allowed"
+    })
+  }
+  
+  if(!priceId){
+    return res.status(400).json({
+          error: "Price not found!"
+        })
+  }
   const successUrl = `${process.env.NEXT_URL}/sucess`;
   const cancelUrl = `${process.env.NEXT_URL}/cancel`;
 

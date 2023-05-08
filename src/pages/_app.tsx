e114ea728407d4b7348a6cf221f0356d5484cf1d
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-08 15:28:59 -0300

    importando o componente da drawer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,12 @@ import Logo from "../assets/logo.svg";
 import { Container, Header } from "../styles/pages/app";
 import { Bag } from "phosphor-react";
 
-import Image from "next/image";
+ import Image from "next/image";
+import { Drawer } from "@mui/material";
 
 GlobalStyles();
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -15,8 +18,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <Image src={Logo} alt=" " />
         <div>
           <Bag size={24}></Bag>
+       
         </div>
       </Header>
+      
       <Component {...pageProps} />
     </Container>
   );

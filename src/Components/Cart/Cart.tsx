e3026430407837dc8576039b7bc20fe08038d876
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-09 15:13:56 -0300

    terminando a estilização do carrinho com os itens

--- src/Components/Cart/Cart.tsx
+++ src/Components/Cart/Cart.tsx
@@ -2,14 +2,19 @@
 
 import Image from "next/image";
 import Camisa1 from "../../assets/Camisas/Camisa 1.png";
-import { ImageContainer, ProductContainer } from "@/src/styles/components/Cart";
+import { ImageContainer, ProductContainer, ProductContent } from "@/src/styles/components/Cart";
 export default function CartItem() {
   return (
     <ProductContainer>
       <ImageContainer>
         <Image src={Camisa1} alt="" width={95} height={95} />
       </ImageContainer>
-      <main>teste</main>
+      <ProductContent>
+      <h4>Camisa Ignite Explorer</h4>
+      <main>R$: 89.90</main>
+      <div>Remover</div>
+      </ProductContent>
+       
     </ProductContainer>
   );
 }
--- src/styles/components/Cart.ts
+++ src/styles/components/Cart.ts
@@ -1,27 +1,58 @@
 import { styled } from "..";
 
 export const ProductContainer = styled("main", {
+  gap: "1rem",
+  margin: "0 auto",
+  maxWidth: 1180,
+  display: "flex",
 
-    gap: "4rem",
-    margin: "0 auto",
-    maxWidth: 1180,
-    display: 'flex',
- 
-   justifyContent: "flex-start",
-   paddingRight:    "6rem",
-  });
-  export const ImageContainer = styled("div", {
-     marginBottom: '1.5rem',
-    width: "100%",
-    maxWidth: 95,
-    height: 95,
-    background: "linear-gradient(100deg, #1ea483 0%, #7465d4 100%) ",
-    borderRadius: 8,
-    padding: "0.25rem",
+  justifyContent: "flex-start",
+  paddingRight: "6rem",
+});
+export const ImageContainer = styled("div", {
+  marginBottom: "1.5rem",
+  width: "100%",
+  maxWidth: 95,
+  height: 95,
+  background: "linear-gradient(100deg, #1ea483 0%, #7465d4 100%) ",
+  borderRadius: 8,
+  padding: "0.25rem",
+  
+  justifyContent: "center",
+  alignItems: "center",
+  img: {
+    objectFit: "cover",
+  },
+});
 
-    justifyContent: "center",
-    alignItems: "center",
-    img: {
-      objectFit: "cover",
-    },
-  });
+export const ProductContent = styled("div", {
+    paddingLeft: 5,
+  maxWidth: 265,
+  width: 200 ,
+  maxHeight: 103,
+  display: "flex",
+  flexDirection: "column",
+ justifyContent: "space-between",
+  h4: {
+   
+    color: '$gray300',
+    fontSize: '$md' ,
+    marginBottom: 1,
+  },
+
+  main: {
+        fontSize: '$lg' ,
+        fontWeight: 'bold',
+
+  },
+
+  div: {
+        fontSize: '$md',
+        color: '$green300' ,
+        cursor: 'pointer',
+        transition: '0.2s' ,
+        '&:hover': {
+            color: 'red' ,
+        }
+  }
+});

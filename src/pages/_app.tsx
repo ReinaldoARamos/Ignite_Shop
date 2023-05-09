53bb81a050370720fcc916e18543a4fe8265239f
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-09 17:29:40 -0300

    adicionando um Cart Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,38 +15,48 @@ import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { useState } from "react";
 
-
- import Image from "next/image";
+import Image from "next/image";
 import { Bag } from "phosphor-react";
 import PopOver from "../Components/PopOver/PopOver";
-
-
-
+import { CartProvider } from "use-shopping-cart";
 
 GlobalStyles();
 
-
-
-
-
 export default function App({ Component, pageProps }: AppProps) {
-
-
   return (
+    <>
+
+
+<CartProvider
+      mode="payment"
+      cartMode="client-only"
+      // Connects to your Stripe account
+      stripe={process.env.STRIPE_PUBLIC_KEY}
+      // Redirected here after successful payments
+      successUrl={`${process.env.NEXT_URL}/success`}
+      // Redirected here when you click back on Stripe Checkout
+      cancelUrl={`${process.env.NEXT_URL}/cancel`}
+      currency="BRL"
+      // Only customers from UK will be able to purchase
+      // Having this setting means that we will capture shipping address
+      allowedCountries={["BR"]}
+      // Enables local storage
+      shouldPersist={true}
+    >
     <Container>
-    
       <Header>
         <Image src={Logo} alt=" " />
 
         <div>
-         <PopOver></PopOver>  
-
-         
+          <PopOver />
         </div>
       </Header>
-  
-     
+
       <Component {...pageProps} />
     </Container>
+    </CartProvider>
+
+    </>
+   
   );
 }
